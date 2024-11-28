45ed57fec6790a39e3534e963219fa6756533cad
Fairuz Ardhan Haunan <120712894+ardhanhau07@users.noreply.github.com>
2024-11-29 03:33:50 +0700

    Map + helicopter

--- Assets/Flooded_Grounds/PostProcessing/Editor/PropertyDrawers/MinDrawer.cs
+++ Assets/Flooded_Grounds/PostProcessing/Editor/PropertyDrawers/MinDrawer.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-using UnityEngine.PostProcessing;
+using UnityEditor;
 
 namespace UnityEditor.PostProcessing
 {
@@ -8,17 +8,18 @@ namespace UnityEditor.PostProcessing
     {
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            MinAttribute attribute = (MinAttribute)base.attribute;
+            // Ambil MinAttribute dari field
+            MinAttribute minAttribute = (MinAttribute)attribute;
 
             if (property.propertyType == SerializedPropertyType.Integer)
             {
                 int v = EditorGUI.IntField(position, label, property.intValue);
-                property.intValue = (int)Mathf.Max(v, attribute.min);
+                property.intValue = (int)Mathf.Max(v, minAttribute.min);
             }
             else if (property.propertyType == SerializedPropertyType.Float)
             {
                 float v = EditorGUI.FloatField(position, label, property.floatValue);
-                property.floatValue = Mathf.Max(v, attribute.min);
+                property.floatValue = Mathf.Max(v, minAttribute.min);
             }
             else
             {
